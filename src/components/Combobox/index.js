2f2ff3f7e1d10246d7a48d9f7e2217c34fc32ac2
Ivan <Ivan@Ivan.com>
2020-11-11 19:54:42 +0100

    adding FontAwsome

--- src/components/Combobox/index.js
+++ src/components/Combobox/index.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
  
 import { languages } from '../../constants/const'
 import ComboboxItem from './ComboboxItem'
@@ -9,15 +11,18 @@ const Combobox = () => {
         languages.find(el => el.name === "eng"))
       const [open, setOpen] = useState(false)
   return (
-    <div className={styles.combobox} onClick={()=>setOpen(!open)}>
-        {open && 
-            <ul>
-                {languages.map(el => 
-                <ComboboxItem {...el} setSelected={setSelectedItem} selected={el.name === selectedItem.name}/>)}
-            </ul>
-        }
-        {selectedItem.language}
-    </div>
+    <div className={styles.combobox} onClick={() => setOpen(!open) }>
+    {<ul style={{display: open ? 'flex' : 'none'}}>
+        {languages.map(el => 
+        <ComboboxItem {...el} 
+          setSelected={setSelectedItem}
+          selected={el.name === selectedItem.name}
+        />)}
+     </ul>
+    }
+    <FontAwesomeIcon icon={faChevronDown} size="0.2x" style={  {paddingRight: "12px"}} />
+    {selectedItem.language}
+</div>
     )
 }
 
