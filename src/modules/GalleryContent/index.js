994db0f02abd298c3cdc9eb6e107e65765662e0e
Ivan <Ivan@Ivan.com>
2020-11-18 20:57:04 +0100

    Refactoring to use map

--- src/modules/GalleryContent/index.js
+++ src/modules/GalleryContent/index.js
@@ -3,12 +3,17 @@ import React from 'react'
 import styles from './style.module.css'
 import GalleryContainer from '../../components/GalleryContainer'
  
+const titles = ["Amazing exterior", "Relaxing sauna", "Modern interior"]
+ 
 const GalleryContent = () => (
-    <main className={styles.galleryContent}>
-        <GalleryContainer title="Amazing exterior" start={0} end={4} isGray />
-        <GalleryContainer title="Relaxing sauna" start={4} end={8} />
-        <GalleryContainer title="Modern interior" start={8} end={24} isGray />
-    </main>
+<main className={styles.galleryContent}>
+    {titles.map((title, index) =>
+        <GalleryContainer title={title} isGray={index % 2 === 0}
+        start={index * 4} end={(index + 1) === 3
+            ? (index + 4) * 4
+            : (index + 1) * 4}/>
+    )}
+</main>
 )
  
 export default GalleryContent
